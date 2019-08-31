6fd7775677a31c8746699448a6f82e9a397fb65a
Tiago Carrondo <tcarrondo@angulosolido.pt>
2019-08-31 13:35:41 +0100

    add timestamp filename creation

--- aws-creds-update.sh
+++ aws-creds-update.sh
@@ -16,6 +16,13 @@ type aws >/dev/null 2>&1 || { printf >&2 "aws is required for this script (snap 
 # preparing jq filter
 jq_filter='.[].Credentials.AccessKeyId,.[].Credentials.SecretAccessKey,.[].Credentials.SessionToken,.[].Credentials.Expiration'
 
+# preparing jq output file
+jq_output_filename="aws-token-creds-"`date +%s`
+
+if [ -f ~/.$jq_output_filename ] ; then
+    rm ~/.$jq_output_filename
+fi
+
 # loading vars file
 DIR="${BASH_SOURCE%/*}"
 
@@ -27,26 +34,28 @@ special_duration=$2
 duration=${special_duration:=$token_duration}
 
 # get the token
-aws sts get-session-token --profile $aws_base_profile --token-code $1 --serial-number $mfa_token_sn --duration-seconds $duration | jq --slurp -r $jq_filter > ~/.new-creds
+aws sts get-session-token --profile $aws_base_profile --token-code $1 --serial-number $mfa_token_sn --duration-seconds $duration | jq --slurp -r $jq_filter > ~/.$jq_output_filename
 
 # update aws profile
-if [ $(sed '1q;d' ~/.new-creds) ]; then
-    aws --profile $aws_update_profile configure set aws_access_key_id $(sed '1q;d' ~/.new-creds)
+if [ $(sed '1q;d' ~/.$jq_output_filename) ]; then
+    aws --profile $aws_update_profile configure set aws_access_key_id $(sed '1q;d' ~/.$jq_output_filename)
 fi
 
-if [ $(sed '2q;d' ~/.new-creds) ]; then
-    aws --profile $aws_update_profile configure set aws_secret_access_key $(sed '2q;d' ~/.new-creds)
+if [ $(sed '2q;d' ~/.$jq_output_filename) ]; then
+    aws --profile $aws_update_profile configure set aws_secret_access_key $(sed '2q;d' ~/.$jq_output_filename)
 fi
 
-if [ $(sed '3q;d' ~/.new-creds) ]; then
-  aws --profile $aws_update_profile configure set aws_session_token $(sed '3q;d' ~/.new-creds)
+if [ $(sed '3q;d' ~/.$jq_output_filename) ]; then
+  aws --profile $aws_update_profile configure set aws_session_token $(sed '3q;d' ~/.$jq_output_filename)
 fi
 
-if [ $(sed '4q;d' ~/.new-creds) ]; then
-    echo "Done! Valid until $(sed '4q;d' ~/.new-creds) (UTC)"
+if [ $(sed '4q;d' ~/.$jq_output_filename) ]; then
+    echo "Done! Valid until $(sed '4q;d' ~/.$jq_output_filename) (UTC)"
 else
     echo "Epic Fail! Please read the docs."
 fi
 
 # final cleanup
-rm ~/.new-creds
+if [ -f ~/.$jq_output_filename ] ; then
+    rm ~/.$jq_output_filename
+fi
