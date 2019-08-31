7ea1683167e770a185ba77e447ab40be2a67e162
Tiago Carrondo <tcarrondo@angulosolido.pt>
2019-08-31 12:31:18 +0100

    better comments and less error messages

--- aws-creds-update.sh
+++ aws-creds-update.sh
@@ -9,8 +9,13 @@
 # then:
 # $ token-aws 000000 [another duration, in seconds, if needed]
 
-type jq >/dev/null 2>&1 || { printf >&2 "jq is required for this script, but it's not installed.  Aborting."; exit 1; }
-type aws >/dev/null 2>&1 || { printf >&2 "aws is required for this script, but it's not installed.  Aborting."; exit 1; }
+# check requirements
+type jq >/dev/null 2>&1 || { printf >&2 "jq is required for this script (snap install jq?), but it's not installed. Aborting."; exit 1; }
+type aws >/dev/null 2>&1 || { printf >&2 "aws is required for this script (snap install aws-cli?), but it's not installed. Aborting."; exit 1; }
+
+# preparing jq filter
+jq_filter='.[].Credentials.AccessKeyId,.[].Credentials.SecretAccessKey,.[].Credentials.SessionToken,.[].Credentials.Expiration'
+
 
 jq_filter='.[].Credentials.AccessKeyId,.[].Credentials.SecretAccessKey,.[].Credentials.SessionToken'
 
@@ -22,8 +27,23 @@ duration=${special_duration:=$token_duration}
 aws sts get-session-token --profile $aws_base_profile --token-code $1 --serial-number $mfa_token_sn --duration-seconds $duration | jq --slurp -r $jq_filter > ~/.new-creds
 
 # update aws profile
+if [ $(sed '1q;d' ~/.new-creds) ]; then
 aws --profile $aws_update_profile configure set aws_access_key_id $(sed '1q;d' ~/.new-creds)
-aws --profile $aws_update_profile configure set aws_secret_access_key $(sed '2q;d' ~/.new-creds)
-aws --profile $aws_update_profile configure set aws_session_token $(sed '3q;d' ~/.new-creds)
+fi
 
+if [ $(sed '2q;d' ~/.new-creds) ]; then
+aws --profile $aws_update_profile configure set aws_secret_access_key $(sed '2q;d' ~/.new-creds)
+fi
+
+if [ $(sed '3q;d' ~/.new-creds) ]; then
+aws --profile $aws_update_profile configure set aws_session_token $(sed '3q;d' ~/.new-creds)
+fi
+
+if [ $(sed '4q;d' ~/.new-creds) ]; then
+    echo "Done! Valid until $(sed '4q;d' ~/.new-creds) (UTC)"
+else
+    echo "Epic Fail! Please read the docs."
+fi
+
+# final cleanup
 rm ~/.new-creds
